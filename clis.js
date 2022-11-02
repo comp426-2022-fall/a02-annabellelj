25340c869f77f1ab10b22fa609066bec5d75a1f7
Annabelle Jiang <annlin@email.unc.edu>
2022-11-02 14:38:07 -0400

    Your Message Here

--- clis.js
+++ clis.js
@@ -58,5 +58,6 @@ if (days == 0) {
 
 if(data.daily.precipitation_hours == 0){
     console.log("You will not need your galoshes");
+} else{
+    console.log("You might need your galoshes")
 }
-console.log("You might need your galoshes")
